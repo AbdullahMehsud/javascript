2604deb70840921db037cf7954524b904088674b
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-06 15:51:09 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs not browser
+
+console.log(3
+    +3) // code readabilty should be high
+console.log("abdullah")
+
+let name = "abdullah"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 power 53
+// bigInt
+// boolen -> true/false
+// symbol -> unique
+// string = ""
+// null -> stand alone
+// undefined -> ex -> let state;
+
+
+// objects
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
