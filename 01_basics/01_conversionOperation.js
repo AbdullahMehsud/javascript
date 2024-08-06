261ede17723e4c5744f8be481652658ec7071fb5
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-06 18:00:32 +0000

    conversion of data types

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,34 @@
+const score = false
+
+//console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueinNumber = Number(score)
+
+// console.log(typeof valueinNumber)
+// console.log(valueinNumber)
+
+// 33 => 33
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1, false => 0
+
+
+let loggedIn = ""
+
+let booleanLoggedIn = Boolean(loggedIn)
+// console.log(booleanLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; "string" => true
+
+let someNumber = 33
+let someString = String(someNumber)
+
+console.log(someString);
+console.log(typeof someString);
+
+// 33 => string
+
