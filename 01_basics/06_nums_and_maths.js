00d6e5cbdf773f8a0ec629d8d21defab7dd5f5ea
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-08 19:57:20 +0000

    nums math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(500)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.93
+
+// console.log(otherNumber.toPrecision(3));
+
+const num = 1000000
+// console.log(num.toLocaleString('en-PK'));
+
+//                          Maths
+
+
+console.log(Math);
+console.log(Math.abs(-4)); // 4
+console.log(Math.round(6.7)); // 7
+console.log(Math.round(6.2)); // 6
+console.log(Math.ceil(4.1)); //5
+console.log(Math.floor(4.9)); //4
