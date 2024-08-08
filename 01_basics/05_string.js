52516c8389fab9285cdae6a71513dfb26bc41d90
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-08 14:46:34 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,12 +6,12 @@ console.log(`my game name is ${name} and price is ${age}`);
 
 const gameName = new String("Abdullah")
 console.log(gameName);
-console.log(gameName[0]);
+console.log(gameName[0]); // to acccess value
 console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toLowerCase());
+console.log(gameName.length); // to find length of string
+console.log(gameName.toLowerCase()); 
 console.log(gameName.toUpperCase());
-console.log(gameName.charAt(3));
+console.log(gameName.charAt(3)); // to find char at 
 console.log(gameName.indexOf("A"));
 
 const newString = gameName.substring(0,4)
@@ -25,5 +25,15 @@ const newStringOne = "       khan      "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
+
+const url = "https://abdullah.com/abdullah%20mehsud"
+
+console.log(url.replace("%20", "-"))
+
+
+console.log(url.includes("abdullah"));
+
+const user = "adnan-ahmad-khan"
+console.log(user.split("-"));
 
 
