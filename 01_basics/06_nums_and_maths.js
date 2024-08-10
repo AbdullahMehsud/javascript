9bf9824f146c35033db68b642f0152daa4d7ba25
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-10 15:43:37 +0000

    nums and math

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -17,12 +17,25 @@ const num = 1000000
 //                          Maths
 
 
-console.log(Math);
-console.log(Math.abs(-4)); // 4
-console.log(Math.round(6.7)); // 7
-console.log(Math.round(6.2)); // 6
-console.log(Math.ceil(4.1)); //5
-console.log(Math.floor(4.9)); //4
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(6.7)); // 7
+// console.log(Math.round(6.2)); // 6
+// console.log(Math.ceil(4.1)); //5
+// console.log(Math.floor(4.9)); //4
 
 
+// console.log(Math.random()); // expected output from 0 to < 1
+// console.log(Math.random() * 10); // 
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+// to define range 
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); 
+
+/// watch video at 1.5 speed 
+ 
 
