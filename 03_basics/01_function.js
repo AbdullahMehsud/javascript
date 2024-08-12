c94815c452058a0f5b14830ef34fc6bb59a8e3e5
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-12 11:09:05 +0000

    function with object and array

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -44,5 +44,40 @@ function loggedInUserMessage(username = "khan"){
 
 //  console.log(loggedInUserMessage("abdullah"));
 //  console.log(loggedInUserMessage());
- console.log(loggedInUserMessage("abdullah"));
+//  console.log(loggedInUserMessage("abdullah"));
+
+
+// spred(...) rest(...)
+// function calculateCartPrice(...num1) {
+//     return num1
+// }
+function calculateCartPrice(val1,val2, ...num1) {
+    return num1
+}
+// console.log(calculateCartPrice(200, 300, 400));
+
+const user = {
+    userName: "abood",
+    age: 24
+}
+
+function handelObj(anyobject) {
+    console.log(`user name ${anyobject.userName} and age is ${anyobject.age}`);
+    
+}
+
+// handelObj(user)
+// handelObj({
+//     userName: "sam",
+//     age: 25
+// })
  
+
+const myNewArray = [0,4,5,7,8]
+
+function returnSecondArray(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondArray(myNewArray));
+console.log(returnSecondArray([200,400,700]));
