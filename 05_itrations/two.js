eaa673dbd053b87d074d7c68918992b842db99de
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-14 18:46:15 +0000

    while, do while loops

---
+++ 05_itrations/two.js
@@ -0,0 +1,22 @@
+let index = 0
+
+while (index <= 10) {
+    // console.log(`value of index: ${index}`);
+    index = index + 2
+    
+}
+
+let myArray = ["superman", "batman", "spiderman"]
+
+let arr = 0
+while (arr < myArray.length ) {
+    // console.log(`this is myArray value: ${myArray[arr]}`);
+    arr++
+}
+
+let score = 0
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
