d91b3c57124bca6563162235c12568b31d6c0b7b
Abdullah Mehsud <124797701+AbdullahMehsud@users.noreply.github.com>
2024-08-15 10:16:27 +0000

    for in loop

---
+++ 05_itrations/three.js
@@ -0,0 +1,42 @@
+// for of 
+
+const array = [1,2,3,4,5]
+
+for (const arr of array) {
+    // console.log(arr);
+    
+}
+
+
+const greetings = "Hello World"
+
+for (const greet of greetings) {
+    // /console.log(`char of greetings: ${greet}`);
+    
+}
+
+// Maps
+
+const map = new Map()
+
+map.set("IN", "INDIA")
+map.set("USA", "UNITED STATES OF AMARICA")
+map.set("FR", "FRANCE")
+// console.log(map);
+
+
+// for (const [key, value] of map) {
+//     console.log(key, " => ", value);
+    
+// }
+
+
+const myObj = {
+    game1: "Takken",
+    game2: "Fifa"
+}
+
+// for (const obj of myObj) {
+//     console.log(obj);
+    
+// }
